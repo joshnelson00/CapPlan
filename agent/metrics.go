b45f1f487a638c5d31130938b22b7f464ba6500e
Josh Nelson <joshuaknelson1202@icloud.com>
2026-01-11 09:32:34 -0700

    added request handling/metric retrieval

--- agent/metrics.go
+++ agent/metrics.go
@@ -1,71 +1,64 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
-	"runtime/metrics"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	//"github.com/prometheus/client_golang/prometheus"
+	//"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-func main() {
-	metricsList, err := loadMetrics()
-	if err != nil {
-		log.Fatal(err)
+func shutdownServers(node *exec.Cmd, prometheus *exec.Cmd) {
+	if node.Process != nil {
+		node.Process.Signal(os.Interrupt)
+		node.Wait()
 	}
-	fmt.Println("┌────────────────────┐")
-	fmt.Println("│  Tracked Metrics   │")
-	fmt.Println("└────────────────────┘")
-	for i := 0; i < len(metricsList); i++ {
-		fmt.Printf("%d. %s\n", i+1, metricsList[i])
+	if prometheus.Process != nil {
+		prometheus.Process.Signal(os.Interrupt)
+		prometheus.Wait()
 	}
-
-	// NEXT STEPS
-	// 1. Track Metrics from "metricsList" slice
-	// 2. Return and store metrics in Structs/Formatted JSON/etc.
 }
 
-func loadMetrics() ([]string, error) {
-	var metricsSlice []string
+func main() {
+	// Start servers
+	nodeServer := exec.Command("../node_exporter/node_exporter")
+	prometheusServer := exec.Command(
+		"../prometheus/prometheus",
+		"--config.file=../prometheus/prometheus.yml",
+	)
 
-	file, err := os.Open("tracked-metrics.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	nodeServer.Start()
+	prometheusServer.Start()
+	defer shutdownServers(nodeServer, prometheusServer)
 
-	scanner := bufio.NewScanner(file)
+	fmt.Println("Running Local HTTP Server...")
 
-	for scanner.Scan() {
-		metricsSlice = append(metricsSlice, scanner.Text())
-	}
+	// HTTP server
+	httpServer := &http.Server{Addr: ":2112"}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Println("Hello From Function 1!")
+	})
 
-	// Name of the metric we want to read.
-	const myMetric = "/cpu/classes/total:cpu-seconds"
-	// Create a sample for the metric.
-	sample := make([]metrics.Sample, 1)
-	sample[0].Name = myMetric
+	// Signal handling
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 
-	// Sample the metric.
-	metrics.Read(sample)
+	go func() {
+		<-sig
+		fmt.Println("\nShutting down...")
+		httpServer.Close() // unblocks ListenAndServe
+	}()
 
-	// Check if the metric is actually supported.
-	// If it's not, the resulting value will always have
-	// kind KindBad.
-	if sample[0].Value.Kind() == metrics.KindBad {
-		panic(fmt.Sprintf("metric %q no longer supported", myMetric))
-	}
+	// Blocks until Close() is called
+	httpServer.ListenAndServe()
 
-	// Handle the result.
-	//
-	// It's OK to assume a particular Kind for a metric;
-	// they're guaranteed not to change.
-	cpuSecs := sample[0].Value
-	fmt.Printf("Seconds CPU spent executing program: %d\n", cpuSecs)
-
-	return metricsSlice, err
+	//NEXT STEPS
+	// 1. Request from http://localhost:9100/metrics
+	// 2. Automate the timing in between (user defined intervals in config or go. RESEARCH)
+	// 3. Print all content from request to the screen in formatted text (will handle business logic later)
+	// 4. Automate the Download of Node Exporter and Prometheus based on Distro/Architecture/
 }
